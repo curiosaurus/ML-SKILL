736da965ebf115136af00ef1cb76c14c27c01e07
Pratik Patil <35042555+curiosaurus@users.noreply.github.com>
2019-08-03 10:16:23 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,25 @@
+
+a=[[1,2,3,4,5,6,7],['a','b','c','d','e','f','g'],[1.1,1.64,1.75,4.20,5.5454,6.945,9.11]]
+z=[1,2,3,4,6,5,7]
+print(a[:-1])
+print(a[-1:])
+print(a[:1])
+print(a[1,2]) for numpy
+print(a[:1][2:3])
+print (a[:2])
+print(len(a))
+print(a[1].index('d'))
+a[0].insert(2,5)
+z[0:2]=['a','b']
+t=('a')
+print(type(t))#now string
+t=('a','b','c')
+print(type(t))#now tuple
+
+z= 'a','b'
+print(type(z))
+cgpa={'a':7.8,'b':6.9,'c':9.11,'d':8.5}
+x={'a':12,'b':345,'a':435}
+print(x['b'])
+x.popitem()
+print('a' is x)
